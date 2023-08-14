80852fa8e424f902bfdd4be78dcb39f996d8597d
Ramon Oliveira Novaes <112136464+ramonoliveira1@users.noreply.github.com>
2023-08-14 15:38:26 -0300

    git commit -m "criando app"

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return(
+        <header className='banner'>
+        <img src="/imgs/banner.png" alt="Banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
